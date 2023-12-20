19d2d366ca89aba239059d2f8e3aba3eca4f138d
Samy Offer <samyoffer@gmail.com>
2023-12-20 11:44:25 +0100

    ajout de 'fin' a la fin de l'affichage des extensions

--- src/Client.java
+++ src/Client.java
@@ -46,12 +46,12 @@ public class Client {
               // to read data from the keyboard
               BufferedReader kb = new BufferedReader(new InputStreamReader(System.in));
 
-              System.out.println("Extensions deja compris dans extensions.txt , réecrivez les si vous souhaiteé les gardez encore zebi : ");
+              System.out.println("Extensions deja compris dans extensions.txt , réecrivez les si vous souhaiteé les gardez encore : ");
               String str;
               while( !(str = br.readLine()).equals("fin") ){
                   System.out.println(str);
               }
-
+              System.out.println("----"+str+"----");
               System.out.print("Entrez les extensions à sauvegarder au format (txt,jpeg,jpg,...) : ");
               String extensionsDeUser = scanner.nextLine();
 
